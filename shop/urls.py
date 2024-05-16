969facce6e38a6f5dc2c0582c52dcb3ca6f848f3
ubuntu <kamoliddinnasirov@mail.ru>
2024-05-16 17:17:38 +0500

    add shop details

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from shop.views import Shop, ProductDetail, WishlistView, update_cart_view, ShoppingCart, wishlist_view
 
+app_name = "shop"
+
 urlpatterns = [
-    path("", Shop.as_view(), name="shop"),
+    path("", Shop.as_view(), name="home"),
     path("product/<int:pk>", ProductDetail.as_view(), name="detail"),
     path("product/<int:pk>/wishlist/", wishlist_view, name="wishlist"),
     path("wishlist/", WishlistView.as_view(), name='all_wishlist'),
